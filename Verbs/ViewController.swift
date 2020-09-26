177c700493a070e395ce4f91fdcf8cb4b06889b3
Oleg Samoilov <16526317+olejiksa@users.noreply.github.com>
2020-09-26 16:05:09 +1000

    Initial Commit

---
+++ Verbs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Verbs
+//
+//  Created by Oleg Samoylov on 26.09.2020.
+//  Copyright © 2020 Oleg Samoylov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
