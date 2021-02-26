ce125ef90bdd379aa97dd4701330316128497956
Oleg Samoilov <16526317+olejiksa@users.noreply.github.com>
2021-02-27 06:09:50 +1000

    Assets for Mac were moved from iOS target

---
+++ VerbsMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  VerbsMac
+//
+//  Created by Oleg Samoylov on 27.02.2021.
+//  Copyright © 2021 Oleg Samoylov. All rights reserved.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
